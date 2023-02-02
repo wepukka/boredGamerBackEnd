6c85d86a24430efa52bce772a0f7915e49a7a013
Anssi Vepsäläinen <83569026+wepukka@users.noreply.github.com>
2023-02-02 12:49:06 +0200

    Mapping with params

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { fetchFreeGames } = require("./fetch");
-const testdata = require("./testdata");
 
 router.use(express.json());
 router.use(express.urlencoded());
@@ -10,12 +9,59 @@ router.get("/", function (req, res) {
   res.json({ msg: "BoredGamer Api" });
 });
 
-router.get("/testi/", async function (req, res) {
-  const data = await fetchFreeGames();
-  // const freeGames = await fetchFreeGames(params);
-  res.send({
-    data,
-  });
+router.get("/games/", async function (req, res) {
+  try {
+    if (!req.query.platform || !req.query.genre) {
+      return res.send({
+        msg: "Invalid params!",
+      });
+    }
+
+    let mappedArray = [];
+    let platform = req.query.platform;
+    let genre = req.query.genre;
+
+    let data = await fetchFreeGames();
+
+    // Based on platform
+    if (genre === "All") {
+      data.map((game) => {
+        if (game.platform === platform) {
+          mappedArray.push(game);
+        }
+      });
+      return res.send({ msg: "Success", data: mappedArray });
+    }
+    // Based on platform and genre
+    else {
+      data.map((game) => {
+        if (game.genre === genre && game.platform === platform) {
+          mappedArray.push(game);
+        }
+      });
+      res.send({ msg: "Success", data: mappedArray });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/genres/", async function (req, res) {
+  try {
+    let data = await fetchFreeGames();
+    let genreArray = [];
+
+    data.map((data) => {
+      if (!genreArray.includes(data.genre)) {
+        genreArray.push(data.genre);
+      }
+    });
+    res.send({
+      genres: genreArray,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
